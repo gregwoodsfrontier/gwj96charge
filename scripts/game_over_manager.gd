eb12c11c4e47fe79d9e597d3a4475cc342892843
gregwoodsfrontier <iwannaplaygamesjp@gmail.com>
2026-09-05 15:20:45 -0400

    game restart after 2s of gameover.

--- scripts/game_over_manager.gd
+++ scripts/game_over_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var game_over_screen: CanvasLayer = $"../GameOverScreen"
 @onready var game_over_label: Label = $"../GameOverScreen/MarginContainer/GameOverLabel"
+@onready var restart_timer: Timer = $"../GameOverScreen/RestartTimer"
 
 func _ready() -> void:
 	game_over_screen.hide()
@@ -10,8 +11,15 @@ func _ready() -> void:
 	var health_comp := player_node.get_health_component()
 	if health_comp:
 		health_comp.death.connect(_on_player_death)
+	
+	restart_timer.timeout.connect(_on_restart_timer_timeout)
+
+func _on_restart_timer_timeout() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 func _on_player_death() -> void:
 	get_tree().paused = true
 	game_over_label.text = GameConstant.GAME_LOSE_TEXT
 	game_over_screen.show()
+	restart_timer.start()
